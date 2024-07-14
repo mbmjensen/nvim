407add6d78bcf524f4c19c9ba8d3a64c7b96095e
Michael Jensen <michaelbmjensen@gmail.com>
2024-07-14 13:35:29 -0700

    Update option settings
    This change updates my neovim options to reflect the settings used in my
    vimrc. In places where the defaults for neovim differed from vim, the
    explicit settings were omitted

--- lua/jensen/options.lua
+++ lua/jensen/options.lua
@@ -1,16 +1,37 @@
-local opt = vim.opt
-
 -- Indent Settings
-opt.autoindent = true
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.softtabstop = 4
+vim.opt.expandtab = true               -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4                 -- Indent with 4 spaces
+vim.opt.softtabstop = 4                -- Count tabs as 4 spaces for editing operations
 
 -- Line Wrap Settings
-opt.breakindent = true  -- Indent wrapped lines
-opt.showbreak= "..."  -- Prefix wrapped rows with 3 dots
-opt.linebreak = true  -- Wrap lines at word boundaries
+vim.opt.breakindent = true             -- Indent wrapped lines
+vim.opt.showbreak= "..."               -- Prefix wrapped rows with 3 dots
+vim.opt.linebreak = true               -- Wrap lines at word boundaries
+
+-- Window Settings
+vim.opt.splitbelow = true              -- New windows go below
+vim.opt.splitright = true              -- and right of a split
+
+vim.opt.laststatus = 3                 -- Display only a single, permanent status bar
+                                       -- at the bottom of the screen
 
 -- Search Settings
-opt.ignorecase = true  -- Make the search case-insensitive by default
-opt.smartcase = true  -- Unless an uppercase character is in the pattern
+vim.opt.ignorecase = true              -- Make the search case-insensitive by default
+vim.opt.smartcase = true               -- unless an uppercase character is in the pattern
+
+vim.opt.hlsearch = false               -- Do not highlight search results after searching
+
+-- List Characters
+vim.opt.list = true                    -- Turn on list characters by default
+vim.opt.listchars = {
+    tab = "» ",                        -- Tab characters, preserve width
+    extends = "›",                     -- Unwrapped text to screen right
+    precedes = "‹",                    -- Unwrapped text to screen left
+    trail = "•",                       -- Trailing spaces
+    nbsp = "•",                        -- Non-breaking spaces
+}
+
+
+-- Misc Settings
+vim.opt.confirm = true                 -- Prompt instead of rejecting commands such as a risky :write
+vim.opt.undofile = true                -- Persist undo history for files between file reads
