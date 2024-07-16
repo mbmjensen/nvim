b50820d57bf9ffba05a8d89604c64a221efaa733
Michael Jensen <michaelbmjensen@gmail.com>
2024-07-16 00:54:25 -0700

    Update PaperColor colorscheme
    This commit fixes the configuration for PaperColor so that it can be
    properly reloaded after loading a different colorscheme. This is done
    using autocommands. This allows custom configuration code to run
    immediately before and immediately after loading the PaperColor theme.
    
    The autocommand before loading the colorscheme is useful for setting
    options that allow the colorscheme to be displayed properly. The
    autocommand afterwards is useful for setting custom color overrides for
    the colorscheme.

--- lua/jensen/plugins/colorscheme.lua
+++ lua/jensen/plugins/colorscheme.lua
@@ -2,9 +2,37 @@ return {
     {
         "https://github.com/NLKNguyen/papercolor-theme.git",
         config = function()
-            vim.opt.termguicolors = true
-            vim.opt.background = "light"
+            -- Always run before loading PaperColor to properly configure colors
+            vim.api.nvim_create_autocmd("ColorSchemePre", {
+                pattern = "PaperColor",
+                callback = function(ev)
+                    vim.opt.termguicolors = true
+                    vim.opt.background = "light"
+                end,
+            })
+
+            -- Apply custom color overrides immediately after loading PaperColor
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                pattern = "PaperColor",
+                callback = function(ev)
+                    vim.cmd.highlight("EndOfBuffer guifg=#b2b2b2 guibg=none")
+                    vim.cmd.highlight("LineNr guibg=none")
+                    vim.cmd.highlight("Normal guibg=none")
+                    vim.cmd.highlight("NormalFloat guibg=none")
+                    vim.cmd.highlight("NormalNC guibg=none")
+
+                    vim.cmd.highlight("IncSearch gui=reverse guifg=none guibg=none")
+                    vim.cmd.highlight("Search gui=reverse guifg=none guibg=none")
+
+                    vim.cmd.highlight("LazyButton guifg=#444444 guibg=none")
+                    vim.cmd.highlight("LazyButtonActive gui=bold guifg=#d75f00 guibg=none")
+                    vim.cmd.highlight("LazyH1 gui=bold guifg=#d75f00 guibg=none")
+                    vim.cmd.highlight("LazyH2 guifg=#005faf guibg=none")
+                end,
+            })
+
+            -- Use PaperColor as the default colorscheme
             vim.cmd.colorscheme("PaperColor")
-        end
+        end,
     },
 }
