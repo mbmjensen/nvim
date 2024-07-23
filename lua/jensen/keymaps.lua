be975e1041c1cf37374775199a40bd964e3f93ac
Michael Jensen <michaelbmjensen@gmail.com>
2024-07-23 01:17:17 -0700

    Update keymaps

--- lua/jensen/keymaps.lua
+++ lua/jensen/keymaps.lua
@@ -30,8 +30,8 @@ vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent 
 vim.keymap.set("n", "yss", "ys_", { remap = true })
 
 -- Make it easier to search through search and command histories
-vim.keymap.set("n", "q/", "<Leader>Telescope search_history<CR>", { desc = "Search / history" })
-vim.keymap.set("n", "q:", "<Leader>Telescope command_history<CR>", { desc = "Search command history" })
+vim.keymap.set("n", "q/", "<CMD>Telescope search_history<CR>", { desc = "Search / history" })
+vim.keymap.set("n", "q:", "<CMD>Telescope command_history<CR>", { desc = "Search command history" })
 
 -- Use s for [s]earch instead of [s]ubstitute
 vim.keymap.set({ "n" }, "s", "<NOP>")
@@ -44,3 +44,81 @@ vim.keymap.set("n", "sh", "<CMD>Telescope help_tags<CR>", { desc = "[S]earch [H]
 -- Git Keymappings
 vim.keymap.set("n", "<Leader>gs", "<CMD>Git<CR>", { desc = "git summary" })
 vim.keymap.set("n", "<Leader>gS", "<CMD>Git | only<CR>", { desc = "git fullscreen-summary" })
+
+-- Option Toggles
+vim.keymap.set("n", "<Leader>ts", "<CMD>set spell!<CR>", { desc = "Toggle spelling" })
+vim.keymap.set("n", "<Leader>tw", "<CMD>set wrap!<CR>", { desc = "Toggle line wrap" })
+vim.keymap.set("n", "<Leader>tn", "<CMD>set number!<CR>", { desc = "Toggle line numbers" })
+vim.keymap.set("n", "<Leader>tg", "<CMD>Gitsigns toggle_signs<CR>", { desc = "Toggle line numbers" })
+
+
+-- Open Keymappings
+vim.keymap.set("n", "<Leader>oz", "<CMD>Lazy<CR>", { desc = "Open Lazy" })
+vim.keymap.set("n", "<Leader>om", "<CMD>Mason<CR>", { desc = "Open Mason" })
+vim.keymap.set("n", "<Leader>oq", "<CMD>copen<CR>", { desc = "Open quickfix" })
+vim.keymap.set("n", "<Leader>ot", "<CMD>startinsert | terminal<CR>", { desc = "Open terminal" })
+
+
+-- LSP keybindings (stolen from kickstarter)
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+	callback = function(event)
+		-- NOTE: Remember that Lua is a real programming language, and as such it is possible
+		-- to define small helper and utility functions so you don't have to repeat yourself.
+		--
+		-- In this case, we create a function that lets us more easily define mappings specific
+		-- for LSP related items. It sets the mode, buffer and description for us each time.
+		local map = function(keys, func, desc)
+			vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+		end
+
+		-- Jump to the definition of the word under your cursor.
+		--  This is where a variable was first declared, or where a function is defined, etc.
+		--  To jump back, press <C-t>.
+		map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+		-- Find references for the word under your cursor.
+		map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+		-- Jump to the implementation of the word under your cursor.
+		--  Useful when your language has ways of declaring types without an actual implementation.
+		map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+		-- Jump to the type of the word under your cursor.
+		--  Useful when you're not sure what type a variable is and you want to see
+		--  the definition of its *type*, not where it was *defined*.
+		map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+		-- Fuzzy find all the symbols in your current document.
+		--  Symbols are things like variables, functions, types, etc.
+		map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+		-- Fuzzy find all the symbols in your current workspace.
+		--  Similar to document symbols, except searches over your entire project.
+		map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+		-- Rename the variable under your cursor.
+		--  Most Language Servers support renaming across files, etc.
+		map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+		-- Execute a code action, usually your cursor needs to be on top of an error
+		-- or a suggestion from your LSP for this to activate.
+		map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+		-- Opens a popup that displays documentation about the word under your cursor
+		--  See `:help K` for why this keymap.
+		map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+		-- WARN: This is not Goto Definition, this is Goto Declaration.
+		--  For example, in C this would take you to the header.
+		map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+		vim.api.nvim_create_autocmd('LspDetach', {
+			group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+			callback = function(event2)
+				vim.lsp.buf.clear_references()
+				vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+			end,
+		})
+	end
+})
