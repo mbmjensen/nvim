098958484639f6bb68826eda174f969b48b7fe7f
Michael Jensen <michaelbmjensen@gmail.com>
2024-07-27 03:14:53 -0700

    Use symbols for diagnostic information in the sign column

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 require("options")
 require("keymaps")
 require("lazy-setup")
+
+-- Change git gutter symbols for diagnostics
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
