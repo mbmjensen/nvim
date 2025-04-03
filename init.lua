26bdb4e4f703d3cad7940ca53ca81b3aed60ee36
Michael Jensen <michaelbmjensen@gmail.com>
2025-04-02 20:46:10 -0700

    Refactor init file
    The new format is easier to read and fixes an issue where lazy was
    loading plugins before the leader key was set. As a result, keymaps that
    were set by plugins were not properly assigned.

--- init.lua
+++ init.lua
@@ -1,10 +1,32 @@
+require("bootstrap").lazy({
+	repo = "https://github.com/folke/lazy.nvim.git",
+	dir = vim.fn.stdpath("data") .. "/lazy/lazy.nvim",
+	branch = "stable", -- The latest stable release
+})
+
 require("options")
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
+	ui = {
+		border = "rounded",
+	},
+	install = {
+		colorscheme = {
+			"github_light",
+			"default",
+		},
+	},
+	change_detection = {
+		enabled = false,
+	},
+})
 require("keymaps")
-require("lazy-setup")
 
 -- Change git gutter symbols for diagnostics
 local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
---
+++ lua/bootstrap.lua
@@ -0,0 +1,32 @@
+M = {}
+
+local function install_lazy(opts)
+	local system_out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable", -- latest stable release
+		opts.repo,
+		opts.dir,
+	})
+
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ system_out,                     "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+
+M.lazy = function(opts)
+	if not vim.uv.fs_stat(opts.dir) then
+		install_lazy(opts)
+	end
+
+	vim.opt.rtp:prepend(opts.dir)
+end
+
+return M
