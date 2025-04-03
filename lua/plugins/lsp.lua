84ef5f6c845176a462e8fa4a49aad4a21e5fec5d
Michael Jensen <michaelbmjensen@gmail.com>
2025-04-02 22:47:53 -0700

    Update deprecated way of defining hover config
    The older 'vim.lsp.with' is deprecated. This commit uses a
    non-deprecated way of configuring the hover popup.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,10 +31,6 @@ return {
 			require("lspconfig").vimls.setup({
 				capabilities = capabilities,
 			})
-
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-				border = "rounded",
-			})
 		end,
 	},
 	{
