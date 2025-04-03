ab156cb3e8b4b70e407ce241587a5480f9417669
Michael Jensen <michaelbmjensen@gmail.com>
2025-04-02 21:18:26 -0700

    Allow config to be used for VSCode Neovim extension
    The VSCode extension, vscode-neovim runs Neovim in the background. As a
    result, many non-editing plugins, commands, and settings do not work as
    they do in Neovim. This change only enables the editing plugins and
    options that work in VSCode.
    
    For reference, documentation on the extension can be found here:
    https://github.com/vscode-neovim/vscode-neovim

--- init.lua
+++ init.lua
@@ -5,28 +5,42 @@ require("bootstrap").lazy({
 })
 
 require("options")
-require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-	},
-	ui = {
-		border = "rounded",
-	},
-	install = {
-		colorscheme = {
-			"github_light",
-			"default",
-		},
-	},
-	change_detection = {
-		enabled = false,
-	},
-})
-require("keymaps")
 
--- Change git gutter symbols for diagnostics
-local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+if vim.g.vscode then
+	-- Neovim is running in the background via the VSCode extension, vscode-neovim. With Neovim is
+	-- running in the background, many settings, commands, or plugins that control Neovim's UI may
+	-- not work as expected. In addition, many commands and key mappings need to be modified to use
+	-- VSCode's API. As a result, limit settings and plugins to those that focus on text editing
+	-- and text objects. For more information on the VSCode API, which can be used to trigger
+	-- commands and set keymaps for VSCode, refer to the extension documentation here:
+	--
+	-- https://github.com/vscode-neovim/vscode-neovim documentation expected.
+	require("lazy").setup({
+		spec = {
+			{ import = "plugins.editing" }
+		},
+		change_detection = {
+			enabled = false,
+		},
+	})
+else
+	require("lazy").setup({
+		spec = {
+			{ import = "plugins" },
+		},
+		ui = {
+			border = "rounded",
+		},
+		install = {
+			colorscheme = {
+				"github_light",
+				"default",
+			},
+		},
+		change_detection = {
+			enabled = false,
+		},
+	})
+	require("keymaps")
+	require("diagnostics")
 end
---
+++ lua/diagnostics.lua
@@ -0,0 +1,6 @@
+-- Change git gutter symbols for diagnostics
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
