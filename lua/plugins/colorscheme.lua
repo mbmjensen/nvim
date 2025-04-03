1faf7fccc3d28d041fee5896236dce5314dac9a1
Michael Jensen <michaelbmjensen@gmail.com>
2025-04-02 22:46:18 -0700

    Remove PaperColor from colorschemes
    This also removes some vestigial code and uncomments some highlight
    groups for the lazy popup in the github colorscheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,58 +1,6 @@
 return {
 	{
-		"https://github.com/NLKNguyen/papercolor-theme.git",
-		lazy = true, -- Ensure that the colorscheme loads before other plugins
-		-- priority = 100, -- which might set their colors using the colorscheme.
-		config = function()
-			-- Always run before loading PaperColor to properly configure colors
-			vim.api.nvim_create_autocmd("ColorSchemePre", {
-				pattern = "PaperColor",
-				callback = function(ev)
-					vim.opt.termguicolors = true
-					vim.opt.background = "light"
-				end,
-			})
-
-			-- Apply custom color overrides immediately after loading PaperColor
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				pattern = "PaperColor",
-				callback = function(ev)
-					vim.cmd.highlight("CursorLineNr guibg=none")
-					vim.cmd.highlight("Conceal guibg=none")
-					vim.cmd.highlight("EndOfBuffer guifg=#b2b2b2 guibg=none")
-					vim.cmd.highlight("LineNr guibg=none")
-					vim.cmd.highlight("NonText guibg=none")
-					vim.cmd.highlight("Normal guibg=none")
-					vim.cmd.highlight("NormalFloat guibg=none")
-					vim.cmd.highlight("NormalNC guibg=none")
-					vim.cmd.highlight("SignColumn guibg=none")
-					vim.cmd.highlight("Whitespace guibg=none")
-
-					vim.cmd.highlight("IncSearch gui=reverse guifg=none guibg=none")
-					vim.cmd.highlight("Search gui=reverse guifg=none guibg=none")
-
-					vim.cmd.highlight("LazyButton guifg=#444444 guibg=none")
-					vim.cmd.highlight("LazyButtonActive gui=bold guifg=#d75f00 guibg=none")
-					vim.cmd.highlight("LazyH1 gui=bold guifg=#d75f00 guibg=none")
-					vim.cmd.highlight("LazyH2 guifg=#005faf guibg=none")
-
-					vim.cmd.highlight("DiagnosticError guibg=none")
-					vim.cmd.highlight("DiagnosticWarn gui=bold guifg=green guibg=none")
-					vim.cmd.highlight("DiagnosticHint gui=bold guifg=green guibg=none")
-					vim.cmd.highlight("LspDiagnosticsDefaultInformation guibg=none")
-
-					-- Add a border underneath the treesitter context.
-					vim.cmd.highlight("TreesitterContextBottom gui=underline guisp=Grey")
-					vim.cmd.highlight("TreesitterContextLineNumberBottom gui=underline guisp=Grey")
-				end,
-			})
-
-			-- Use PaperColor as the default colorscheme
-			-- vim.cmd.colorscheme("PaperColor")
-		end,
-	},
-	{
-		'projekt0n/github-nvim-theme',
+		'https://github.com/projekt0n/github-nvim-theme.git',
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
@@ -64,11 +12,11 @@ return {
 
 			vim.api.nvim_create_autocmd("ColorScheme", {
 				pattern = "github_light",
-				callback = function(ev)
-					-- vim.cmd.highlight("LazyButton guifg=#444444 guibg=none")
-					-- vim.cmd.highlight("LazyButtonActive gui=bold guifg=#d75f00 guibg=none")
-					-- vim.cmd.highlight("LazyH1 gui=bold guifg=#d75f00 guibg=none")
-					-- vim.cmd.highlight("LazyH2 guifg=#005faf guibg=none")
+				callback = function()
+					vim.cmd.highlight("LazyButton guifg=#444444 guibg=none")
+					vim.cmd.highlight("LazyButtonActive gui=bold guifg=#d75f00 guibg=none")
+					vim.cmd.highlight("LazyH1 gui=bold guifg=#d75f00 guibg=none")
+					vim.cmd.highlight("LazyH2 guifg=#005faf guibg=none")
 
 					-- Add a border underneath the treesitter context.
 					vim.cmd.highlight("TreesitterContextBottom gui=underline guisp=Grey")
@@ -80,12 +28,6 @@ return {
 					vim.cmd.highlight("link MasonMutedBlock Normal")
 					vim.cmd.highlight("link MasonHeader Bold")
 
-					-- vim.api.nvim_set_hl(0, "@ibl.indent.char.1", {
-					-- 	fg = "#d0d7de",
-					-- })
-					-- vim.api.nvim_set_hl(0, "@ibl.whitespace.char.1", {
-					-- 	fg = "#d0d7de",
-					-- })
 					vim.api.nvim_set_hl(0, "IblIndent", {
 						fg = "#afb8c1",
 					})
